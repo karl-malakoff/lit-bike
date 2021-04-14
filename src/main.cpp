ed7f9a116f01b0a5e86ba8446f11baa7d21f60c6
Karl Malakoff <karltheawesome@gmail.com>
2021-04-15 07:55:24 +1000

    Flashing leds working!

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include "FastLED.h"
-#include "OneButton.h"
 
 // PWM led pins
 const int PARTY_LED_PIN = 5;
@@ -25,11 +24,10 @@ const int LEFT_INDICATOR_PIN = 2;
 const int RIGHT_INDICATOR_PIN = 8;
 const int PARTY_PIN = 7;
 
-// TODO - replace this with something better...
-// OneButton rightIndicator(8, true);
-// OneButton partyButton(7, true);
 
 typedef void (*callback)(void);
+typedef void (*updateFunk)(void);
+// TODO - maybe - make it so update funks are called automatically
 
 class Button
 {
@@ -50,6 +48,7 @@ Button::Button(int pin, unsigned long debounceDelay = 50)
 {
   this->pin = pin;
   this->debounceDelay = debounceDelay;
+  pinMode(pin, INPUT);
 }
 
 void Button::AttachClick(callback onClick) {
@@ -86,18 +85,84 @@ Button leftButton(LEFT_INDICATOR_PIN);
 Button rightButton(RIGHT_INDICATOR_PIN);
 Button partyButton(PARTY_PIN);
 
-int leftButtonState = 0;
-int lastLeftButtonState = LOW;
+class LedFlasher {
+private:
+  int ledPin;
+  bool flashing = false;
+  unsigned long flashDelay = 500;
+  int currentState = LOW;
+  unsigned long lastTick = 0;
+public:
+  LedFlasher(int pin, unsigned long flashDelay = 500);
+  void turnOn();
+  void turnOff();
+  void change(bool on);
+  int getState();
+  void update();
+};
 
-// the following variables are unsigned longs because the time, measured in
-// milliseconds, will quickly become a bigger number than can be stored in an int.
-unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+LedFlasher::LedFlasher(int pin, unsigned long flashDelay = 500){
+  this->ledPin = pin;
+  this->flashDelay = flashDelay;
+  pinMode(pin, OUTPUT);
+}
 
+void LedFlasher::turnOn() {
+  this->lastTick = millis();
+  this->flashing = true;
+  this->currentState = HIGH;
+  digitalWrite(this->ledPin, this->currentState);
+}
 
-const int RIGHT_LED_PIN = 15;
+void LedFlasher::turnOff() {
+  this->lastTick = millis();
+  this->flashing = false;
+  this->currentState = LOW;
+  digitalWrite(this->ledPin, this->currentState);
+}
+
+void LedFlasher::change(bool on) {
+  if (on && !this->flashing) {
+    this->turnOn();
+  } else if (!on && this->flashing) {
+    this->turnOff();
+  }
+}
+
+int LedFlasher::getState() {
+  return this->currentState;
+}
+
+void LedFlasher::update() {
+  if (this->flashing){
+    long int now = millis();
+    if ((now - this->lastTick) > this->flashDelay) {
+      this->currentState = this->currentState == HIGH ? LOW : HIGH;
+      digitalWrite(this->ledPin, this->currentState);
+      this->lastTick = now;
+    }
+  }
+}
+
+const int RIGHT_LED_PIN = 16;
 const int LEFT_LED_PIN = 14;
 const int PARTY_LED_INDICATOR_PIN = 6;
+
+LedFlasher rightFlasher(RIGHT_LED_PIN);
+LedFlasher leftFlasher(LEFT_LED_PIN);
+LedFlasher partyFlasher(PARTY_LED_INDICATOR_PIN);
+
+struct BikeState
+{
+  bool leftIndicating = false;
+  bool rightIndicating = false;
+  bool partying = false;
+
+  // Changing indicates that the state machine should transition
+  bool changing = false;
+};
+
+BikeState state;
 
 // temp
 bool ledOn;
@@ -107,11 +172,19 @@ int ticks = 0;
 CHSV backColor = CHSV(0, 244, 244);
 
 void leftIndicatorClick() {
-  static int click = 0;
-  Serial.print("clicked ");
-  Serial.println(click++);
-  ledOn = !ledOn;
-  change = true;
+  state.changing = true;
+  state.leftIndicating = !state.leftIndicating;
+}
+
+void rightIndicatorClick() {
+  state.changing = true;
+  state.rightIndicating = !state.rightIndicating;
+}
+
+void partyIndicatorClick() {
+  state.changing = true;
+  // TODO - this should change the party mode
+  state.partying = !state.partying;
 }
 
 void setLeds(CRGB* leds, int num, CRGB color){
@@ -123,13 +196,17 @@ void setLeds(CRGB* leds, int num, CRGB color){
 
 void setup() {
   // Setup all our pins:
-  pinMode(LEFT_INDICATOR_PIN, INPUT);
-  pinMode(RIGHT_INDICATOR_PIN, INPUT);
-  pinMode(PARTY_PIN, INPUT);
+  // pinMode(LEFT_INDICATOR_PIN, INPUT);
+  // pinMode(RIGHT_INDICATOR_PIN, INPUT);
+  // pinMode(PARTY_PIN, INPUT);
 
-  pinMode(LEFT_LED_PIN, OUTPUT); 
-  pinMode(RIGHT_LED_PIN, OUTPUT); 
-  pinMode(PARTY_LED_INDICATOR_PIN, OUTPUT); 
+  // pinMode(LEFT_LED_PIN, OUTPUT); 
+  // pinMode(RIGHT_LED_PIN, OUTPUT); 
+  // pinMode(PARTY_LED_INDICATOR_PIN, OUTPUT); 
+
+  // digitalWrite(LEFT_LED_PIN, HIGH);
+  // digitalWrite(RIGHT_LED_PIN, HIGH);
+  // digitalWrite(PARTY_LED_INDICATOR_PIN, HIGH);
 
   FastLED.addLeds<NEOPIXEL, BACK_LED_PIN>(backLeds, TOT_NUM_LED_BACK);
   FastLED.addLeds<NEOPIXEL, PARTY_LED_PIN>(partyLeds, NUM_LED_PARTY);
@@ -140,12 +217,18 @@ void setup() {
 
   // attach callbacks
   leftButton.AttachClick(leftIndicatorClick);
+  rightButton.AttachClick(rightIndicatorClick);
+  partyButton.AttachClick(partyIndicatorClick);
 }
 
 void loop() {
   leftButton.Update();
   rightButton.Update();
   partyButton.Update();
+
+  leftFlasher.update();
+  rightFlasher.update();
+  partyFlasher.update();
 
   // testing code for left button (pin 4)
   // int leftReading = digitalRead(2);
@@ -173,6 +256,22 @@ void loop() {
     setLeds(backLeds, TOT_NUM_LED_BACK, color);
     FastLED.show();
     change = false;
+  }
+
+  if (state.changing) {
+    // we just do all the changes, slightly inefficient but whatevs
+
+    Serial.println("changing!");
+    // TODO these should actually flash
+    // digitalWrite(LEFT_LED_PIN, state.leftIndicating);
+    // digitalWrite(RIGHT_LED_PIN, state.rightIndicating);
+    // digitalWrite(PARTY_LED_INDICATOR_PIN, state.partying);
+
+    leftFlasher.change(state.leftIndicating);
+    rightFlasher.change(state.rightIndicating);
+    partyFlasher.change(state.partying);
+
+    state.changing = false;
   }
 
   // Always flash the back leds
